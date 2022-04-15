f887b5a1a796a59506f8b3a1e7386f524eb82d80
VladislavSinenko <synenkovladyslav@gmail.com>
2022-04-15 14:38:30 +0300

    about section added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { MasterHeadComponent } from './components/master-head/master-head.component';
 import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     MasterHeadComponent,
-    PortfolioComponent
+    PortfolioComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
