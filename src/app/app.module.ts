bfeb0d76655257f56d605db0cc051ff4a669e0f4
VladislavSinenko <synenkovladyslav@gmail.com>
2022-04-15 14:27:08 +0300

    porfolio component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { MasterHeadComponent } from './components/master-head/master-head.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    MasterHeadComponent
+    MasterHeadComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule
