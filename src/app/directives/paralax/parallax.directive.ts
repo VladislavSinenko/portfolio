ed394ebc0591c308cdc28789c794d10b7f8dcfe8
VladislavSinenko <synenkovladyslav@gmail.com>
2022-05-14 19:42:29 +0300

    optimize paralax-directive

--- src/app/directives/paralax/parallax.directive.ts
+++ src/app/directives/paralax/parallax.directive.ts
@@ -6,16 +6,13 @@ import { Directive, Input, ElementRef, HostListener } from '@angular/core';
 export class ParallaxDirective {
   @Input('ratio') parallaxRatio: number = 1;
   private elementStyleRef: any;
-  [index: number]: string;
 
   constructor(private eleRef: ElementRef) {
     this.elementStyleRef = this.eleRef.nativeElement.style;
-    this.elementStyleRef.willChange = "transform";
   }
 
   @HostListener("window:scroll")
   onWindowScroll() {
-    let scrollY = ~~window.scrollY;
-    this.elementStyleRef.transform = this[scrollY] ?? (this[scrollY] = "translateY(" + (scrollY * this.parallaxRatio) + "px)");
+    this.elementStyleRef.transform = `translateY(${~~(window.scrollY * this.parallaxRatio)}px)`;
   }
 }
